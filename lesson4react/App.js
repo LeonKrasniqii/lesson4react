1ae689db05940134e275303716260fc62f1541b0
LeonKrasniqii <redonpc14@gmail.com>
2026-02-12 20:21:14 +0100

    n

--- lesson4react/App.js
+++ lesson4react/App.js
@@ -1,18 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {createAppContainer} from ' react-navigation';
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import ListScreen from './screems/ListScreen';
 
-const navigator = createStackNavigator(
-  {
-  List: ListScreen
-},
-{
-initialRouteName: 'List',
-defaultNavigationOptions:{
-  title: 'App'
-}
-}
-);
-export default createAppContainer(navigator);
+import ListScreen from './screens/ListScreen';
+const Stack = createStackNavigator();
+
+export default function App() {
+   return (
+    <NavigationContainer>
+      <Stack.Navigator
+      initialRouteName="List"
+      screenOptions={{
+        headerTitle:'App',
+      }}
+    >
+        <Stack.Screen name="List" component={ListScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+   );
+  }
--- lesson4react/screens/ListScreen.js
+++ lesson4react/screens/ListScreen.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import {Text,StyleSheet,View,FlatList} from 'react-native';
+import React from "react";
+import { Text, StyleSheet, View, FlatList} from "react-native";
 
-const studens = [
-    {name:'Jon',surname:'Behra',age:'17'},
-    {name:'Leon',surname:'Krasniqi',age:'16'},
-    {name:'Dion',surname:'Ibrahimi',age:'17'},
-    {name:'Germainum',surname:'Morina',age:'26'},
+
+const students = [
+    {name: 'Jon', surname: 'Behra', age: '17'},
+    {name: 'Leo', surname: 'Krasniqi', age: '16'},
+    {name: 'Dion', surname: 'Ibrahimi', age: '17'},
+    {name: 'Germanium', surname: 'Morina', age: '26'}
 ]
-const ListScreen = () =>{
-    return(
-        <View>
-            <Text style={styles.textStyle}>List of Screens</Text>
+
+
+const ListScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.textStyle}>List of Students</Text>
             <FlatList
-                data={studens}
+                data={students}
                 horizontal={true}
                 showsHorizontalScrollIndicator={false}
                 keyExtractor={student => student.name}
-                renderItem={({item}) => {
+                renderItem={({item})=> {
                     return <Text style={styles.studentsWrapper}>{item.name} {item.surname} - {item.age}</Text>
                 }}
             />
@@ -24,6 +27,29 @@ const ListScreen = () =>{
     )
 };
 
-const styles = StyleSheet.create({});
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#fff',
+    },
+    textStyle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    studentsWrapper: {
+        fontSize: 20,
+        marginRight: 20,
+        padding: 10,
+        backgroundColor: '#f0f0f0',
+        borderRadius: 5,
+        width: 300,
+    },
+});
+
 
 export default ListScreen;
